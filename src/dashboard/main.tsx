859484d499d7db1f6f992d54ff456514d3aca073
Alex Kibet <111179230+Fivezerone@users.noreply.github.com>
2026-06-19 11:53:45 +0000

    chore: remove Figma template directories from extension source

--- src/dashboard/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import Dashboard from "./Dashboard";
-import "../shared/styles/index.css";
-
-createRoot(document.getElementById("root")!).render(<Dashboard />);
--- src/popup/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import Popup from "./Popup";
-import "../shared/styles/index.css";
-
-createRoot(document.getElementById("root")!).render(<Popup />);
